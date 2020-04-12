52324af305530764d4e137a58ab89e41623b96a5
Sachin negi <san.negi.77777@gmail.com>
2020-04-12 16:40:19 +0530

    </StylesProvider> added in adminpanel.jsx

--- src/components/AdminPanel/AdminPanel.jsx
+++ src/components/AdminPanel/AdminPanel.jsx
@@ -6,36 +6,39 @@ import Typography from "@material-ui/core/Typography";
 import useStyles from "./AdminPanelStyle";
 
 import SideBar from "./SideBar";
+import { StylesProvider } from "@material-ui/core/styles";
 
 function AdminPanel(props) {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      {/* <SideBar /> */}
-      <SideBar />
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-        {[1, 2, 3, 4].map((val) => (
-          <Typography paragraph key={val}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-            dolor purus non enim praesent elementum facilisis leo vel. Risus at
-            ultrices mi tempus imperdiet. Semper risus in hendrerit gravida
-            rutrum quisque non tellus. Convallis convallis tellus id interdum
-            velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean
-            sed adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-            integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-            eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-            quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-            vivamus at augue. At augue eget arcu dictum varius duis at
-            consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-            donec massa sapien faucibus et molestie ac.
-          </Typography>
-        ))}
-      </main>
-    </div>
+    <StylesProvider injectFirst>
+      <div className={classes.root}>
+        <CssBaseline />
+        {/* <SideBar /> */}
+        <SideBar />
+        <main className={classes.content}>
+          <div className={classes.toolbar} />
+          {[1, 2, 3, 4].map((val) => (
+            <Typography paragraph key={val}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Rhoncus dolor purus non enim praesent elementum facilisis leo vel.
+              Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+              gravida rutrum quisque non tellus. Convallis convallis tellus id
+              interdum velit laoreet id donec ultrices. Odio morbi quis commodo
+              odio aenean sed adipiscing. Amet nisl suscipit adipiscing bibendum
+              est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+              Metus vulputate eu scelerisque felis imperdiet proin fermentum
+              leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt
+              lobortis feugiat vivamus at augue. At augue eget arcu dictum
+              varius duis at consectetur lorem. Velit sed ullamcorper morbi
+              tincidunt. Lorem donec massa sapien faucibus et molestie ac.
+            </Typography>
+          ))}
+        </main>
+      </div>
+    </StylesProvider>
   );
 }
 
